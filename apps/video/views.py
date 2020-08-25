c4db8fa08f601f9277ec355867e1d6a851e2215e
Juan <kaenel10@gmail.com>
2020-08-25 01:48:23 -0300

    Eliminar video

--- apps/video/views.py
+++ apps/video/views.py
@@ -30,34 +30,47 @@ class ListVideo(APIView):
         return Response(video_json.errors, status=400)
 
 
-
 class DetailVideo(APIView):
+    def get_object(self, pk):
+        """
+        Filtra los videos existentes
+        """
+        try:
+            return Video.objects.get(pk=pk)  # filtrame por id
+        except Video.DoesNotExist:  # si no existe el video
+            raise Http404
+
     def get(self, request, pk):
         """
         endpoint: Me permite obtener el detalle de un video
         :return un video en formato json. Si no existe devuelve un 404
         """
         try:
-            video = Video.objects.get(pk=pk)  # filtrame por id
+            video = self.get_object(pk)
             video_json = VideoSerializer(video)  # serializamos un único objeto
             return Response(video_json.data)
-        except Video.DoesNotExist: #si no existe el video
+        except Video.DoesNotExist:  # si no existe el video
             raise Http404
 
-    def put(self,request,pk):
+    def put(self, request, pk):
         """
         endpoint: Me permite actualizar el detalle de un video
         :return un video en formato json. Si no existe devuelve un 404
          """
         try:
-            video = Video.objects.get(pk=pk)  # filtrame por id
-            video_json = VideoSerializer(video,data=request.data)  #Acá VideoSerializer toma todos los datos que nuestro usr nos envie y los almacena en el objeto video
+            video = self.get_object(pk)  # filtrame por id
+            video_json = VideoSerializer(video,
+                                         data=request.data)  # Acá VideoSerializer toma todos los datos que nuestro usr nos envie y los almacena en el objeto video
             if video_json.is_valid():
                 video_json.save()
                 return Response(video_json.data)
-            #else
+            # else
             return Response(video_json.errors, status=400)
 
-        except Video.DoesNotExist: #si no existe el video
+        except Video.DoesNotExist:  # si no existe el video
             raise Http404
 
+    def delete(self, request, pk):
+        video = self.get_object(pk)
+        video.delete()
+        return Response(status=204) #status 204 -> no encontró ningun contenido
