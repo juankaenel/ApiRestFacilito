20ddee4fca5013c380d0792f71a63ba91fbaf3b0
Juan <kaenel10@gmail.com>
2020-08-25 01:38:42 -0300

    endpoint obtener detalle de un video y actualizar un video

--- apps/video/urls.py
+++ apps/video/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import ListVideo
+from .views import ListVideo,DetailVideo
 urlpatterns = [
     url(r'^videos/$', ListVideo.as_view(), name='lista-video'),
+    url(r'^videos/(?P<pk>[0-9]+)$', DetailVideo.as_view(), name='detail-video'),
 ]
--- apps/video/views.py
+++ apps/video/views.py
@@ -1,20 +1,63 @@
-#rest
+# rest
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-#apps
+# apps
 from .models import Video
 from .serializers import VideoSerializer
 
-#endpoint
+
 class ListVideo(APIView):
-    def get(self,request):
+    def get(self, request):
+        """
+        endpoint: Me permite obtener todos los videos
+        :return videos en formato json
+        """
         videos = Video.objects.all()
-        video_json = VideoSerializer(videos, many=True) #MarshalL -> serializamos el objeto. Esta clase nos regresará un objeto serializado, el campo many es pq son varios objetos q se serializan
+        video_json = VideoSerializer(videos,
+                                     many=True)  # MarshalL -> serializamos el objeto. Esta clase nos regresará un objeto serializado, el campo many es pq son varios objetos q se serializan
         return Response(video_json.data)
 
-    def post(self,request):
-        video_json =  VideoSerializer(data=request.data, many=True) #UnMarshall
-        if video_json.is_valid():#si lo q nos envió el usr es correcto
-            video_json.save() #guardamos en la bd
-            return Response(video_json.data,status=201) #se creó un nuevo registro le damos el status 201
-        return Response(video_json.errors,status=400)
+    def post(self, request):
+        """
+        endpoint: Me permite crear un video
+        :return video en formato json
+        """
+        video_json = VideoSerializer(data=request.data, many=True)  # UnMarshall
+        if video_json.is_valid():  # si lo q nos envió el usr es correcto
+            video_json.save()  # guardamos en la bd
+            return Response(video_json.data, status=201)  # se creó un nuevo registro le damos el status 201
+        return Response(video_json.errors, status=400)
+
+
+
+class DetailVideo(APIView):
+    def get(self, request, pk):
+        """
+        endpoint: Me permite obtener el detalle de un video
+        :return un video en formato json. Si no existe devuelve un 404
+        """
+        try:
+            video = Video.objects.get(pk=pk)  # filtrame por id
+            video_json = VideoSerializer(video)  # serializamos un único objeto
+            return Response(video_json.data)
+        except Video.DoesNotExist: #si no existe el video
+            raise Http404
+
+    def put(self,request,pk):
+        """
+        endpoint: Me permite actualizar el detalle de un video
+        :return un video en formato json. Si no existe devuelve un 404
+         """
+        try:
+            video = Video.objects.get(pk=pk)  # filtrame por id
+            video_json = VideoSerializer(video,data=request.data)  #Acá VideoSerializer toma todos los datos que nuestro usr nos envie y los almacena en el objeto video
+            if video_json.is_valid():
+                video_json.save()
+                return Response(video_json.data)
+            #else
+            return Response(video_json.errors, status=400)
+
+        except Video.DoesNotExist: #si no existe el video
+            raise Http404
+
